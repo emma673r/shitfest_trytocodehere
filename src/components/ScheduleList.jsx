2eba53940f6065b656a400bd0e7f387f8cbb3844
emma673r <90177139+emma673r@users.noreply.github.com>
2022-12-09 11:18:46 +0100

    band list works

--- src/components/ScheduleList.jsx
+++ src/components/ScheduleList.jsx
@@ -1,15 +1,17 @@
-import Schedule from "./Schedule";
+// import Schedule from "./Schedule";
 
-function ScheduleList(props) {
-  console.log(props);
-  return (
-    <main className="ScheduleList">
-      {props.schedule.map((schedule) =>
-        // <Schedule key={schedule.mon} schedule={schedule} />
-        console.log("schedule", schedule)
-      )}
-    </main>
-  );
-}
+// function ScheduleList(props) {
+//   console.log(props);
+//   return (
+//     <main className="ScheduleList">
+//       {Object.values(props.schedule).map(
+//         (schedule) => (
+//           <Schedule key={schedule.mon} schedule={schedule} />
+//         )
+//         // console.log("schedule", schedule)
+//       )}
+//     </main>
+//   );
+// }
 
-export default ScheduleList;
+// export default ScheduleList;
